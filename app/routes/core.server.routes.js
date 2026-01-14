275b40ad2020a31cf403f1f1bcb251039cfc79f7
khaledhusain <khaledhusain21@gmail.com>
2026-01-14 16:11:37 +0300

    Protect item creation and bidding routes with authentication middleware

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -1,16 +1,17 @@
 const core = require("../controllers/core.server.controllers");
+const authenticate = require("../lib/authentication");
 
 module.exports = function(app) {
     app.route("/search")
         .get(core.search_items);
 
     app.route("/item")
-        .post(core.create_item);
+        .post(authenticate, core.create_item);
 
     app.route("/item/:item_id")
         .get(core.get_item_by_id);
 
     app.route("/item/:item_id/bid")
-        .post(core.place_bid)
+        .post(authenticate, core.place_bid)
         .get(core.get_bids_for_item);
 };
